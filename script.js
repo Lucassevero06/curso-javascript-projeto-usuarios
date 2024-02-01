2f0a017ffcbc67d192518e656d50543b01f10fa7
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-01 15:04:45 -0400

    cancelando o envio do formulário com o preventDefault

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
+document.getElementById("form-user-create").addEventListener("submit", function(event){
 
-    if (field.name == "gender") {
+    event.preventDefault();
 
-        if (field.checked === true) {
+    fields.forEach(function(field, index){
+
+        if (field.name == "gender") {
+    
+            if (field.checked === true) {
+                user[field.name] = field.value;
+            }
+    
+        } else {
+    
             user[field.name] = field.value;
+    
         }
+    
+    });
 
-    } else {
+    console.log(user);
 
-        user[field.name] = field.value;
-
-    }
-
-});
-
-console.log(user);
+})
